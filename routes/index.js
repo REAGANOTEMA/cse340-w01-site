b65d16b8ecd203cbdef6325572a2f8921a122927
ReaganOtema <reaganotema2022@gmail.com>
2025-11-07 12:19:19 +0300

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// Home page route
 router.get("/", (req, res) => {
-  res.render("index"); // No layout call needed
+  res.render("index", { 
+    name: "Guest",      // You can replace this dynamically if needed
+    title: "CSE Motors" // Page title for <head>
+  });
 });
 
 module.exports = router;
